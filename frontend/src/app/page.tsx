255d309a739cd98cbbfef37c3a8de34bd2ba506a
トーマス <210441057@ccmailg.meijo-u.ac.jp>
2023-12-01 22:02:10 +0900

    feat:ログイン機能

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -7,18 +7,11 @@ export default async function Home() {
     const user = session?.user // ログインしていなければnullになる。
 
     return (
-        <main
-            style={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                height: "70vh",
-            }}
-        >
-            <div>
-                {user ? <div>Logged in</div> : <div>Not logged in</div>}
+        <header className="flex font-mono w-1440 h-85 p-10 md:p-25 justify-between items-center flex-shrink-0 bg-gradient-to-r from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90% text-white">
+            <p className="text-6xl not-italic font-normal leading-normal">Eventer</p>
+            <div className="flex flex-col items-start gap-20">
                 {user ? <LogoutButton/> : <LoginButton/>}
             </div>
-        </main>
+        </header>
     );
 }
